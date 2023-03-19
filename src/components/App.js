47650b3e1495b779bcbfd38a7b279621473d6461
Kishanvaghasiya108 <122118895+Kishanvaghasiya108@users.noreply.github.com>
2023-03-19 15:30:08 +0530

    fupdated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,49 +5,41 @@ const App = () => {
 
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sortAscending, setSortAscending] = useState(true);
+  const [sortAscending, setSortAscending] = useState(true);  
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchData = async () => {
       setIsLoading(true);
-      try {
-        const response = await fetch("https://content.newtonschool.co/v1/pr/main/users");
-        const data = await response.json();
-        setUsers(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUsers();
+      const response = await fetch('https://content.newtonschool.co/v1/pr/main/users');
+      const data = await response.json();
+      setUsers(data);
+      setIsLoading(false);
+    }
+    fetchData();
   }, []);
 
   const handleSort = () => {
-    setSortAscending(!sortAscending);
-    const sortedUsers = [...users].sort((a, b) => {
-      const nameA = a.name.length;
-      const nameB = b.name.length;
-      if (nameA < nameB) {
-        return sortAscending ? -1 : 1;
-      }
-      if (nameA > nameB) {
-        return sortAscending ? 1 : -1;
-      }
-      return 0;
-    });
-    setUsers(sortedUsers);
-  };
+    if (sortAscending) {
+      const sortedUsers = [...users].sort((a, b) => a.name.length - b.name.length);
+      setUsers(sortedUsers);
+      setSortAscending(false);
+    } else {
+      const sortedUsers = [...users].sort((a, b) => b.name.length - a.name.length);
+      setUsers(sortedUsers);
+      setSortAscending(true);
+    }
+  }
 
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn" onClick={() => setIsLoading(true)}>Fetch User Data</button>
+      <button className="fetch-data-btn" onClick={handleSort}>Fetch User Data</button>
       <button className="sort-btn" onClick={handleSort}>
         {sortAscending ? "Sort by name length (ascending)" : "Sort by name length (descending)"}
       </button>
-      {isLoading ? <p>Loading...</p> :
-        <ul className='users-section'>
-          {users.map(user => (
+      {isLoading ? <p>Loading...</p> : (
+        <div className='users-section'>
+          {users.map((user) => (
             <li key={user.id}>
               <section className='id-section'>{user.id}</section>
               <section className='name-email-section'>
@@ -56,8 +48,8 @@ const App = () => {
               </section>
             </li>
           ))}
-        </ul>
-      }
+        </div>
+      )}
     </div>
   )
 }
