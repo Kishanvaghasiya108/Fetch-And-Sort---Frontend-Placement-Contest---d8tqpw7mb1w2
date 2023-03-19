d597ca18a9f99248d31224911a560e4d55aceec4
Kishanvaghasiya108 <122118895+Kishanvaghasiya108@users.noreply.github.com>
2023-03-19 15:37:33 +0530

    final update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,33 @@ const App = () => {
   const [sortAscending, setSortAscending] = useState(true);  
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch('https://content.newtonschool.co/v1/pr/main/users');
-      const data = await response.json();
-      setUsers(data);
-      setIsLoading(false);
-    }
-    fetchData();
+    setIsLoading(true);
+    fetch('https://content.newtonschool.co/v1/pr/main/users')
+      .then(response => response.json())
+      .then(data => {
+        setIsLoading(false);
+        setUsers(data);
+      })
+      .catch(error => console.error(error));
   }, []);
+
+  const handleFetchData = () => {
+    setIsLoading(true);
+    fetch('https://content.newtonschool.co/v1/pr/main/users')
+      .then(response => response.json())
+      .then(data => {
+        setIsLoading(false);
+        setUsers(data);
+      })
+      .catch(error => console.error(error));
+  }
 
   const handleSort = () => {
     if (sortAscending) {
-      const sortedUsers = [...users].sort((a, b) => a.name.length - b.name.length);
-      setUsers(sortedUsers);
+      setUsers(users.slice().sort((a, b) => a.name.length - b.name.length));
       setSortAscending(false);
     } else {
-      const sortedUsers = [...users].sort((a, b) => b.name.length - a.name.length);
-      setUsers(sortedUsers);
+      setUsers(users.slice().sort((a, b) => b.name.length - a.name.length));
       setSortAscending(true);
     }
   }
@@ -33,13 +42,14 @@ const App = () => {
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn" onClick={handleSort}>Fetch User Data</button>
+      <button className="fetch-data-btn" onClick={handleFetchData}>Fetch User Data</button>
       <button className="sort-btn" onClick={handleSort}>
         {sortAscending ? "Sort by name length (ascending)" : "Sort by name length (descending)"}
       </button>
-      {isLoading ? <p>Loading...</p> : (
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && 
         <div className='users-section'>
-          {users.map((user) => (
+          {users.map(user => (
             <li key={user.id}>
               <section className='id-section'>{user.id}</section>
               <section className='name-email-section'>
@@ -49,7 +59,7 @@ const App = () => {
             </li>
           ))}
         </div>
-      )}
+      }
     </div>
   )
 }
